808888a8ca8f46b2aecbef8bae1a4449bce5a93e
LukaVieira1 <lukavieira14@gmail.com>
2025-06-15 16:49:19 -0300

    add: cypress lib and cypress configuration

---
+++ frontend/codes/cypress/support/e2e.ts
@@ -0,0 +1,11 @@
+import "./commands";
+
+Cypress.on("uncaught:exception", (err) => {
+  if (err.message.includes("ResizeObserver loop limit exceeded")) {
+    return false;
+  }
+  if (err.message.includes("Non-Error promise rejection captured")) {
+    return false;
+  }
+  return true;
+});
